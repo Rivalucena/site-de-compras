67b01904aa1dbf6d2e2c608ca65ab5d5319be587
rivaslucena <rivasaraujo@outlook.com>
2023-03-08 21:36:35 -0300

    adicionando images

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const cs = (el)=>document.querySelector(el);
 lancheJson.map((item, index)=>{
     let lancheItem = c('.modelo .lanche-item').cloneNode(true);
     
+    lancheItem.querySelector('.lanche-item--img img').src = item.img;
+
     lancheItem.querySelector('.lanche-item--name').innerHTML = item.name;
     lancheItem.querySelector('.lanche-item--desc').innerHTML = item.description;
     lancheItem.querySelector('.lanche-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
