e1f384afdd727f4b4ff84be9fd1b3d9244dc1c23
rivaslucena <rivasaraujo@outlook.com>
2023-03-08 21:30:49 -0300

    selecionando itens

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const cs = (el)=>document.querySelector(el);
 
 lancheJson.map((item, index)=>{
     let lancheItem = c('.modelo .lanche-item').cloneNode(true);
-    // preencher as informaçoes em lancheitem
+    
+    lancheItem.querySelector('.lanche-item--name').innerHTML = item.name;
+    lancheItem.querySelector('.lanche-item--desc').innerHTML = item.description;
+    lancheItem.querySelector('.lanche-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
 
     c('.lanche-area').append( lancheItem );
 });
