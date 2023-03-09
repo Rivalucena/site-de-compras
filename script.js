c55c41a8664cd167fb6c8c7253bc34c48eab8a49
rivaslucena <rivasaraujo@outlook.com>
2023-03-09 20:58:56 -0300

    lancheWindowArea

--- script.js
+++ script.js
@@ -9,6 +9,14 @@ lancheJson.map((item, index)=>{
     lancheItem.querySelector('.lanche-item--name').innerHTML = item.name;
     lancheItem.querySelector('.lanche-item--desc').innerHTML = item.description;
     lancheItem.querySelector('.lanche-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    lancheItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault();
+        c('lancheWindowArea').style.opacity = 0;
+        c('.lancheWindowArea').style.display = 'flex';
+        setTimeout(()=>{
+            c('.lancheWindowArea').style.opacity = 1;
+        }, 200);
+    });
 
     c('.lanche-area').append( lancheItem );
 });
