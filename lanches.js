67d0dd9c11b2e98b40984a2a40ef21f0852ea62b
rivaslucena <rivasaraujo@outlook.com>
2023-03-09 21:48:34 -0300

    description

--- lanches.js
+++ lanches.js
@@ -1,7 +1,7 @@
 let lancheJson = [
     {id:1, name:'Buffalo Bill', img:'images/Combo-lanches1.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Descrição do lanche em mais de uma linha muito legal bem interessante'},
     {id:2, name:'Tremendão', img:'images/Combo-lanches2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição do lanche em mais de uma linha muito legal bem interessante'},
-    {id:3, name:'Paraiba', img:'images/Combo-lanches3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Descrição do lanche em mais de uma linha muito legal bem interessante'},
+    //{id:3, name:'Paraiba', img:'images/Combo-lanches3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Descrição do lanche em mais de uma linha muito legal bem interessante'},
     {id:4, name:'Americana', img:'images/Combo-lanches4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Descrição do lanche em mais de uma linha muito legal bem interessante'},
     {id:5, name:'Jhonny Deep', img:'images/Combo-lanches5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Descrição do lanche em mais de uma linha muito legal bem interessante'},
     {id:6, name:'Moda da Casa', img:'images/Combo-lanches6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Descrição do lanche em mais de uma linha muito legal bem interessante'},
--- script.js
+++ script.js
@@ -6,12 +6,19 @@ lancheJson.map((item, index)=>{
     
     lancheItem.querySelector('.lanche-item--img img').src = item.img;
 
+    lancheItem.setAttribute('data-key', index);
     lancheItem.querySelector('.lanche-item--name').innerHTML = item.name;
     lancheItem.querySelector('.lanche-item--desc').innerHTML = item.description;
     lancheItem.querySelector('.lanche-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     lancheItem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault();
-        c('lancheWindowArea').style.opacity = 0;
+        let key = e.target.closest('.lanche-item').getAttribute('data-key');
+
+        c('.lancheBig img').src = lancheJson[key].img;
+        c('.lancheInfo h1').innerHTML = lancheJson[key].name;
+        c('.lancheInfo--desc').innerHTML = lancheJson[key].description;
+        
+        c('.lancheWindowArea').style.opacity = 0;
         c('.lancheWindowArea').style.display = 'flex';
         setTimeout(()=>{
             c('.lancheWindowArea').style.opacity = 1;
